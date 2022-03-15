159a088f00ebb2e63131657d009f9dc7da298aae
guada <martinezsguada@gmail.com>
2022-03-15 18:20:17 -0300

    deploy

--- server.js
+++ server.js
@@ -1,14 +1,8 @@
-const path = require('path');
 const express = require('express');
+const path = require('path');
 const app = express();
-
-// Serve static files
-app.use(express.static(__dirname + '/dist/TO-DO-APP'));
-
-// Send all requests to index.html
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/TO-DO-APP/index.html'));
-});
-
-// default Heroku port
-app.listen(process.env.PORT || 5000);
+app.use(express.static(__dirname + '/dist/<to-do-app>'));
+app.get('/*', function(req,res) {
+res.sendFile(path.join(__dirname+
+'/dist/<to-do-app>/index.html'));});
+app.listen(process.env.PORT || 8080);
