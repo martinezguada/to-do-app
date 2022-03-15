0d42591c04433b94998ed2c08edd014abe9b2295
guada <martinezsguada@gmail.com>
2022-03-15 18:05:04 -0300

    make it better

---
+++ src/app/Todo.ts
@@ -0,0 +1,4 @@
+export class Todo{
+    nombre: string;
+    completado: boolean;
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Todo } from './Todo';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'to-do-app';
+  todos : Todo[] = [];
+  nuevoTodo: string;
+
+  saveTodo(){
+    if(this.nuevoTodo){
+    let todo = new Todo();
+    todo.nombre = this.nuevoTodo;
+    todo.completado = true;
+    this.todos.push(todo);
+    this.nuevoTodo = '' ;
+
+  }
+  else{
+    alert('Por favor, agregar una tarea.');
+  }
+}
+
+
+
+realizado(id:number){
+  this.todos[id].completado = !this.todos[id].completado;
+}
+
+borrar(id:number){
+  this.todos = this.todos.filter((v,i)=> i !==id);
+}
 }
