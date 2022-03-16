2dcf6526ffff51e880c1019ed9717777cb57f27d
guada <martinezsguada@gmail.com>
2022-03-16 10:28:37 -0300

    configuracion para heroku

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/<to-do-app>'));
+app.use(express.static(__dirname + '/dist/to-do-app'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/<to-do-app>/index.html'));});
-app.listen(process.env.PORT || 8080);
+'/dist/to-do-app/index.html'));});
+app.listen((process.env.PORT || 8080),()=>{
+    console.log("server escuchando");
+});
